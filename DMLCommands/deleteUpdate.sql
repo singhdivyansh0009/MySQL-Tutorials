3ab66a46816cb0696b72e4a75bb3c3d82f70ff0e
singhdivyansh0009 <=>
2024-08-02 01:34:34 +0530

    change spelling mistake in deleteUpdate file

--- DMLCommands/deleteUpdate.sql
+++ DMLCommands/deleteUpdate.sql
@@ -10,6 +10,6 @@ delete * from Account;
 
 -- update command is used to modify the records of the table
 update Account set balance += 10 where name = "Rita";  
--- if we donot use the where clause all the data will be updated of that 
+-- if we don't use the where clause all the data will be updated of that 
 -- particular coloumn for which we are setting the value
 update Account set balance += 10;
