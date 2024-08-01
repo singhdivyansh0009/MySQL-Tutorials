83e3b20b93839d8107fe162edde518ca9c3bb817
singhdivyansh0009 <=>
2024-08-02 01:32:54 +0530

    DQL commands added

---
+++ DQLCommands/select.sql
@@ -0,0 +1,6 @@
+-- select statement used to retrive the data from the table and return the result set
+select * from Account; -- retrive all the data an return the result set
+select name from Account; -- retrive data of column name
+select name from Account where balance > 1000; -- conditional selection of data
+
+-- here result set can be whole table or can be single data
